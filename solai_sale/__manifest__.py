c1418744067e460887c45282dcb34f5356a1733a
Shelton Freddy <shelton.freddy@oneteam.us>
2020-09-25 17:04:52 -0400

    solai_sale initial commit
    intaller id in sale

---
+++ solai_sale/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Greenhouse Sale Customisation",
+    "author": "One Team US LLC.",
+    "version": "1.0",
+    "summary": "Greenhouse Sale Customisation",
+    "data": [
+        'views/sale.xml',
+
+    ],
+    'depends': ['sale'],
+    'installable': True,
+    'application': True,
+}
---
+++ solai_sale/models/sale.py
@@ -0,0 +1,13 @@
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    installer_id = fields.Many2one('res.users', string='Installer')
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    installer = fields.Boolean('Installer')
