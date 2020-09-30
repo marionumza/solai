746e603f239b8547e55f79ff7e2e57fad1236bbb
Shelton Freddy <shelton.freddy@oneteam.us>
2020-09-30 14:07:24 -0400

    portal order page
    portal order page for installer

--- solai_sale/controllers/portal.py
+++ solai_sale/controllers/portal.py
@@ -15,6 +15,27 @@ from odoo.osv import expression
 
 class CustomerPortal(CustomerPortal):
 
+    def _prepare_home_portal_values(self):
+        values = super(CustomerPortal, self)._prepare_home_portal_values()
+        partner = request.env.user.partner_id
+
+        SaleOrder = request.env['sale.order']
+        quotation_count = SaleOrder.search_count([('state', 'in', ['sent', 'cancel']), '|',
+            ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
+                                                  ('installer_id', '=', request.env.user.id)
+        ])
+        order_count = SaleOrder.search_count([('state', 'in', ['sale', 'done']), '|',
+            ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
+                                              ('installer_id', '=', request.env.user.id)
+
+        ])
+
+        values.update({
+            'quotation_count': quotation_count,
+            'order_count': order_count,
+        })
+        return values
+
     #
     # Quotations and Sales Orders
     #
@@ -70,3 +91,54 @@ class CustomerPortal(CustomerPortal):
             'sortby': sortby,
         })
         return request.render("sale.portal_my_quotations", values)
+
+    @http.route(['/my/orders', '/my/orders/page/<int:page>'], type='http', auth="user", website=True)
+    def portal_my_orders(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
+        values = self._prepare_portal_layout_values()
+        partner = request.env.user.partner_id
+        SaleOrder = request.env['sale.order']
+
+        domain = [('state', 'in', ['sale', 'done']),'|',
+            ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
+                  ('installer_id', '=', request.env.user.id)
+        ]
+
+        searchbar_sortings = {
+            'date': {'label': _('Order Date'), 'order': 'date_order desc'},
+            'name': {'label': _('Reference'), 'order': 'name'},
+            'stage': {'label': _('Stage'), 'order': 'state'},
+        }
+        # default sortby order
+        if not sortby:
+            sortby = 'date'
+        sort_order = searchbar_sortings[sortby]['order']
+
+        archive_groups = self._get_archive_groups('sale.order', domain) if values.get('my_details') else []
+        if date_begin and date_end:
+            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+
+        # count for pager
+        order_count = SaleOrder.search_count(domain)
+        # pager
+        pager = portal_pager(
+            url="/my/orders",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
+            total=order_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # content according to pager and archive selected
+        orders = SaleOrder.search(domain, order=sort_order, limit=self._items_per_page, offset=pager['offset'])
+        request.session['my_orders_history'] = orders.ids[:100]
+
+        values.update({
+            'date': date_begin,
+            'orders': orders.sudo(),
+            'page_name': 'order',
+            'pager': pager,
+            'archive_groups': archive_groups,
+            'default_url': '/my/orders',
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
+        })
+        return request.render("sale.portal_my_orders", values)
