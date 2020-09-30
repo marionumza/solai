e9153a7f8b6163113a522c3edae21d61fb766e8e
Shelton Freddy <shelton.freddy@oneteam.us>
2020-09-30 13:27:50 -0400

    installer portal changes
    show installer orders in portal

---
+++ solai_sale/controllers/portal.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import logging
+from odoo import fields
+from odoo.http import request, content_disposition
+import werkzeug
+import io
+# from cStringIO import StringIO
+
+
+from odoo import http, _
+from odoo.addons.auth_signup.models.res_users import SignupError
+from odoo.addons.web.controllers.main import ensure_db, Home
+from odoo.addons.web_settings_dashboard.controllers.main import WebSettingsDashboard as Dashboard
+from odoo.exceptions import UserError
+from odoo.http import request
+import odoo
+from odoo.exceptions import AccessError, MissingError, ValidationError
+
+_logger = logging.getLogger(__name__)
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
+
+import os
+import odoo
+from operator import itemgetter
+from odoo import fields as odoo_fields, tools, _
+
+
+class CustomerPortal(CustomerPortal):
+
+    #
+    # Quotations and Sales Orders
+    #
+
+    @http.route(['/my/quotes', '/my/quotes/page/<int:page>'], type='http', auth="user", website=True)
+    def portal_my_quotes(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
+        values = self._prepare_portal_layout_values()
+        partner = request.env.user.partner_id
+        SaleOrder = request.env['sale.order']
+
+        domain = [('state', 'in', ['sent', 'cancel']),'|'
+            ('message_partner_ids', 'child_of', [partner.commercial_partner_id.id]),
+                  ('installer_id', '=', request.env.user.id)
+        ]
+
+        searchbar_sortings = {
+            'date': {'label': _('Order Date'), 'order': 'date_order desc'},
+            'name': {'label': _('Reference'), 'order': 'name'},
+            'stage': {'label': _('Stage'), 'order': 'state'},
+        }
+
+        # default sortby order
+        if not sortby:
+            sortby = 'date'
+        sort_order = searchbar_sortings[sortby]['order']
+
+        archive_groups = self._get_archive_groups('sale.order', domain) if values.get('my_details') else []
+        if date_begin and date_end:
+            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+
+        # count for pager
+        quotation_count = SaleOrder.search_count(domain)
+        # make pager
+        pager = portal_pager(
+            url="/my/quotes",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
+            total=quotation_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # search the count to display, according to the pager data
+        quotations = SaleOrder.search(domain, order=sort_order, limit=self._items_per_page, offset=pager['offset'])
+        request.session['my_quotations_history'] = quotations.ids[:100]
+
+        values.update({
+            'date': date_begin,
+            'quotations': quotations.sudo(),
+            'page_name': 'quote',
+            'pager': pager,
+            'archive_groups': archive_groups,
+            'default_url': '/my/quotes',
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
+        })
+        return request.render("sale.portal_my_quotations", values)
