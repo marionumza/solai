87e7c8f6c680a49038818dbf92be7b046b7661ce
Shelton Freddy <shelton.freddy@oneteam.us>
2020-09-30 13:50:53 -0400

    invalid imports removed

--- solai_sale/controllers/portal.py
+++ solai_sale/controllers/portal.py
@@ -1,29 +1,16 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-import logging
-from odoo import fields
-from odoo.http import request, content_disposition
-import werkzeug
-import io
-# from cStringIO import StringIO
 
+import binascii
+from datetime import date
 
-from odoo import http, _
-from odoo.addons.auth_signup.models.res_users import SignupError
-from odoo.addons.web.controllers.main import ensure_db, Home
-from odoo.addons.web_settings_dashboard.controllers.main import WebSettingsDashboard as Dashboard
-from odoo.exceptions import UserError
+from odoo import fields, http, _
+from odoo.exceptions import AccessError, MissingError
 from odoo.http import request
-import odoo
-from odoo.exceptions import AccessError, MissingError, ValidationError
-
-_logger = logging.getLogger(__name__)
+from odoo.addons.payment.controllers.portal import PaymentProcessing
+from odoo.addons.portal.controllers.mail import _message_post_helper
 from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
-
-import os
-import odoo
-from operator import itemgetter
-from odoo import fields as odoo_fields, tools, _
+from odoo.osv import expression
 
 
 class CustomerPortal(CustomerPortal):
